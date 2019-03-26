68c159e31e9710b72da07cb6e5b84282174bf10f
Hakan Vural <41724498+vuraldesign@users.noreply.github.com>
2019-03-27 00:32:38 +0300

    Update and rename temp.sh to temp

--- temp.sh
+++
@@ -1,6 +0,0 @@
-celsius1=$(cat /sys/class/thermal/thermal_zone1/temp | sed 's/.\{3\}$/.&/')
-celsius2=$(cat /sys/class/thermal/thermal_zone2/temp | sed 's/.\{3\}$/.&/')
-echo "--------------------"
-echo "CPU Temp: ${celsius1} °C"
-echo "GPU Temp: ${celsius2} °C"
-echo "--------------------"
